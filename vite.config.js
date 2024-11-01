b34b9a2a66aa06d99534c8d3df3bf67ce53fde8c
Vijay Singh <rockcodingrookie21@gmail.com>
2024-11-01 20:38:14 +0530

    gh-page_setup

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio-website/', // replace 'repo-name' with your repository name
   plugins: [react()],
+  
 })
