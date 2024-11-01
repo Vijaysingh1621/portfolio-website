5a664fb61c73ce0959e0b679ae24324e7f36ed6c
Vijay Singh <rockcodingrookie21@gmail.com>
2024-11-01 17:34:56 +0530

    images_add

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 // App.js
 import React from "react";
 import Header from "./Components/Header";
-import Hero from "./Components/Hero";
 import About from "./Components/about";
 import Projects from "./Components/Projects";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
+import Home from "./Components/Home";
 
 function App() {
   return (
-    <div className="bg-gray-50 text-gray-900">
+    <div className="w-full overflow-hidden">
       <Header />
       <main>
-        <Hero />
+        <Home />
         <About />
         <Projects />
         <Contact />
